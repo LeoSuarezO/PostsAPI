ac61a2ee90d8fec3935893c3125076b4217e90ff
leoSuarezO <leonardo901.ls2@gmail.com>
2023-10-06 17:17:38 -0500

    Method sortedList updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/postsapi", {
@@ -15,108 +15,98 @@ mongoose
     console.error("MongoDB connection error: ", err);
   });
 
-  app.use(express.json());
-  app.use(cors()); 
+app.use(express.json());
+app.use(cors());
 
-  const postSchema = new mongoose.Schema({
-    postId: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    authorId: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    title: String,
-    isPublished: Boolean,
-    createdAt: Date,
-    updatedAt: Date,
-    publishedAt: Date,
-  });
+const postSchema = new mongoose.Schema({
+  postId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  authorId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  title: String,
+  isPublished: Boolean,
+  createdAt: Date,
+  updatedAt: Date,
+  publishedAt: Date,
+});
 
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/api/posts", async (req, res) => {
-    try {
-      const posts = await Post.find();
-      res.json(posts);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-  
-  app.post("/api/create", async (req, res) => {
-    try {
-        console.log(req.body)
-      const {
-        postId,
-        authorId,
-        title,
-        isPublished
-      } = req.body;
-      console.log(req.body);
-      const createdAt = new Date();
-      const post = new Post({
-        postId,
-        authorId,
-        title,
-        isPublished,
-        createdAt
-      });
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-      if(post.isPublished){
-        post.publishedAt = new Date();
-        }
+app.post("/api/create", async (req, res) => {
+  try {
+    const { postId, authorId, title, isPublished } = req.body;
+    const createdAt = new Date();
+    const post = new Post({
+      postId,
+      authorId,
+      title,
+      isPublished,
+      createdAt,
+    });
 
-      await post.save();
-      res.status(200).json(post);
-    } catch (error) {
-        console.log(error)
-      res.status(500).json(error);
+    if (post.isPublished) {
+      post.publishedAt = new Date();
     }
-  }); 
-  
-  app.put("/api/update:/postID", async (req, res) => {
-    try {
-      const postId = req.params.postId;
-      const { authorId, title, isPublished, updatedAt } = req.body;
-      const post = await Post.findOne(postId);
-  
-      post.authorId = authorId;
-      post.title = title;
-      post.isPublished = isPublished;
-      post.updatedAt = updatedAt;
-  
-      await post.save();
-  
-      res.status(200).json(post);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-  
-  app.get("/api/postById/:postId", async (req, res) => {
-    try {
-      const postId = req.params.postId;
-      const post = await Post.findOne(postId);
-      res.status(200).json(post);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
-  
-  app.get("/api/sortedList", async (req, res) => {
-      try{
-          const posts = Post.find({}).sort({title: 1});
-          res.status(200).json(posts);
-      }catch(error){
-          res.status(500).send(error);
-      }
-  });
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
+app.put("/api/update:/postID", async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const { authorId, title, isPublished, updatedAt } = req.body;
+    const post = await Post.findOne(postId);
 
+    post.authorId = authorId;
+    post.title = title;
+    post.isPublished = isPublished;
+    post.updatedAt = updatedAt;
+
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get("/api/postById/:postId", async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const post = await Post.findOne(postId);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get("/api/sortedList", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ title: 1 });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error);
+  }
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
